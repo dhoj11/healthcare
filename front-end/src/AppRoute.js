b0e2557cff6bf90bc8f765327a80d8f861fcf9df
psm7047 <psm7047@naver.com>
2021-06-15 21:30:57 +0900

    add administration

--- front-end/src/AppRoute.js
+++ front-end/src/AppRoute.js
@@ -1,10 +1,12 @@
 import { Redirect, Route , Switch } from "react-router-dom";
 import Home from "./views/Home";
+import Administration from "./views/Administration";
 
 function AppRoute(){
   return(
     <Switch>
       <Route path="/" exact component={Home}/>
+      <Route path="/administration" exact component={Administration}/>
       <Redirect to="/"/>
     </Switch>
   );
---
+++ front-end/src/views/Administration/index.js
@@ -0,0 +1,30 @@
+import "./index.css";
+import Appointment from "./Appointment";
+import PatientInformation from "./PatientInformation";
+import Reception from "./Reception";
+import TestList from "./TestList";
+import SearchPatient from "./SearchPatient";
+
+function Administration(props) {
+  return (
+    <div className="first-content row">
+      <div>
+        <div className="second-content">
+          <div className="appointment-component"><Appointment /></div>
+          <div className="reception-component"><Reception /></div>
+        </div>
+        <div className="testlist-component">
+          <TestList />
+        </div>
+      </div>
+      <div>
+        <div className="search-patient-component"><SearchPatient /></div>
+        <div className="patient-information-component"><PatientInformation /></div>
+        
+      </div>
+    </div>
+    
+  );
+}
+
+export default Administration;
