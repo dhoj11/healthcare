26af70967eedc7b24920ea51fec9046ba2fe49f6
JOUNHO <dnsgh9177@naver.com>
2021-06-17 14:13:09 +0900

    Update appointment

--- front-end/src/views/Appointment/TimeTable/PatientSearch.js
+++ front-end/src/views/Appointment/TimeTable/PatientSearch.js
@@ -1,38 +1,61 @@
+import { useState } from "react";
 import { AutoSizer, List } from "react-virtualized";
 import { getPatientList } from "../data";
-import "./PatientSearch.css";
+import styles from "./PatientSearch.module.css";
 function PatientSearch(props) {
-  const patientList = getPatientList();
 
+  const [patientList, setPatientList]= useState(getPatientList());
   const rowRenderer = ({index, key,style}) => {
     console.log(key,index);
     return (
-      <div key={key} style={style} className="tbody">
+      <div key={key} style={style} className={styles.tbody} onClick={(e) => selectPatient(e,patientList[index].patient_name)}>
+        
           <span>
-          {patientList[index].patient_name}
-          </span>
-          <span>
-          {patientList[index].patient_gender}
-          </span>
-          <span>
-          {patientList[index].patient_birth}
-          </span>
-          <span>
-          {patientList[index].patient_tel}
-          </span>
-          <span>
-          {patientList[index].date}
-          </span>        
+            {patientList[index].patient_name}
+            </span>
+            <span>
+            {patientList[index].patient_gender}
+            </span>
+            <span>
+            {patientList[index].patient_birth}
+            </span>
+            <span>
+            {patientList[index].patient_tel}
+            </span>
+            <span>
+            {patientList[index].date}
+            </span>     
+             
       </div>
     );
   };
 
+  const searchName = (e) => {
+      console.log(e.target.value)
+      const result= getPatientList().filter(data => data.patient_name.includes(e.target.value));
+      console.log(result);
+      setPatientList(result);
+      if(e.target.value===""){
+        setPatientList(getPatientList());
+      }
+    //rowRenderer();
+  }
+
+  const selectPatient = (e,name) => {
+    console.log(name);
+    console.log(e);
+    if(e.currentTarget.style.backgroundColor==="red"){
+      e.currentTarget.style.backgroundColor="";
+    }else{
+      e.currentTarget.style.backgroundColor ="red";
+    }
+  } 
   return(
     <>
     <div className="modal_body">
-              <div className="search"><input type="text" class="search__input" placeholder="Search" onChange/></div>
-              <div className="patient_table">
-                <div className="thead"><span>이름</span><span>성별</span><span>생년월일</span><span>전화번호</span><span>최근내원일</span></div>
+              <div className={styles.search}><input type="text" className={styles.search__input} placeholder="Search" onChange={searchName}/></div>
+              <div className={styles.patient_table}>
+                <div className={styles.thead}><span>이름</span><span>성별</span><span>생년월일</span><span>전화번호</span><span>최근내원일</span></div>
                 <AutoSizer disableHeight>
                   {({width, height}) => {
                     return (
@@ -48,8 +71,8 @@ function PatientSearch(props) {
               </div>
             </div>
             <div className="modal_footer">
-              <button className="btn cancel_btn" onClick={props.handleClose}>취소</button>
-              <button className="btn appoint_btn" onClick={props.handleClose}>예약</button>
+              <button className={styles.cancel_btn} onClick={props.handleClose}>취소</button>
+              <button className={styles.appoint_btn} onClick={props.handleClose}>예약</button>
             </div>
     </>
   );
