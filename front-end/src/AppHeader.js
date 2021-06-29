dc638dd2c1908eb9968511cb31e7633814cb1b34
comdoller <nearermg@gmail.com>
2021-06-29 17:11:41 +0900

    update header

--- front-end/src/AppHeader.js
+++ front-end/src/AppHeader.js
@@ -1,26 +1,48 @@
 import RssParser from 'rss-parser';
 
+import { faHospital, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 function AppHeader(props){
 
-  // async function parseRss () {
-  //   const rssParser = new RssParser()
-  //   const feed = await rssParser.parseURL('http://www.doctorsnews.co.kr/rss/clickTop.xml')
-  //   feed.items.forEach((item) => {
-  //     console.log(item);
-  //   })
-  // }
+  let contents = [];
+  let parser = new RssParser();
+  parser.parseURL('https://cors-anywhere.herokuapp.com/http://www.doctorsnews.co.kr/rss/clickTop.xml', function(err, feed) {
+    if (err) throw err;
+     feed.items.forEach(function(entry) {
+      contents.push( {title: entry.title, link: entry.link} )
+    })
+  })
 
-  // parseRss();
+  var curContent;
+  let length = contents.length;
+  let i=0;
 
+  setTimeout( () => {
+
+    for(let index in contents){
+      curContent = contents[index]
+      console.log(curContent);
+    }
+  }, 2000);
 
   return(
     <div className="appHeader">
       <div className="hospitalTitle">
+        <FontAwesomeIcon icon={faHospital} className="hospitalIcon"/>
         <span className="name">사조병원</span>
+      </div>
+      <div className="newContents">
+        <span>{curContent && curContent.title}</span>
+      </div>
+      <div className="loginUser">
+        <span className="name">의사 : 조운호</span>
+        <span className="logOut"><FontAwesomeIcon icon={faSignOutAlt} className="logOutIcon"/>log-out</span>
       </div>
     </div>
   );
 }
 
 export default AppHeader;
+
