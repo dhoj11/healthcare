1df7b7bc5b0c4cab3ba01dedaa5bc6f67d92ed36
psm7047 <psm7047@naver.com>
2021-07-19 23:28:27 +0900

    for pull

--- front-end/src/apis/auth.js
+++ front-end/src/apis/auth.js
@@ -1,7 +1,6 @@
-import axios from "axios";
+import auth from './axiosConfig';
 
 export function login(staff) {
-  //const promise = axios.post("http://kosa3.iptime.org:50004/auth/login", staff);
-  const promise = axios.post("http://localhost:8080/auth/login", staff);
+  const promise = auth.post("/auth/login", staff);
   return promise;
 }
