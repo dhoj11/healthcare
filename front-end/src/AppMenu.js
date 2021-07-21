f6b4bd37ffc3025837607736b7e75098dd52bd15
psm7047 <psm7047@naver.com>
2021-07-21 14:29:01 +0900

    for pull

--- front-end/src/AppMenu.js
+++ front-end/src/AppMenu.js
@@ -1,13 +1,17 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function AppMenu(props){
   const {chatToggle} = props;
+  const location = useLocation().pathname;
+  console.log(location);
+
   return(
     <>
       <ul className="nav flex-column" >
         <li className="nav-item mt-5">
-          <Link to="/" className="nav-link mt-2"><i className="fas fa-sign-in-alt"></i></Link>
-          <Link to="/dashBoard" className="nav-link mt-2"><i className="fas fa-home"></i></Link>
+          <Link to="/" className="nav-link mt-2"><i className="fas fa-home"></i></Link>
+          <Link to="/dashBoard" className="nav-link"><i className="fas fa-desktop"></i></Link>
           <Link to="/appointment" className="nav-link"><i className="far fa-calendar"></i></Link>
           <Link to="/administration" className="nav-link"><i className="far fa-file-alt"></i></Link>
           <Link to="/treatment" className="nav-link"><i className="fas fa-stethoscope"></i></Link>
